eb8bf1e41a07321231d653d4816a15a31d62729d
WWWolkodaWWW <80260640+WWWolkodaWWW@users.noreply.github.com>
2021-11-14 20:49:45 +0100

    feat: add modal window

--- friday_coding/src/index.js
+++ friday_coding/src/index.js
@@ -1,13 +1,48 @@
+import { Article } from "./js/Article";
+import { ArticleModal } from "./js/ArticleModal";
+import { Modal } from "./js/Modal";
 
-
-
+const data = [
+	{
+		id: 1,
+		title: 'Increasing Prosperity With Positive Thinking',
+		urlToImage: './src/img/images/image1.jpg',
+		tags: ['Art', 'Design'],
+		content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+		date: '01.01.2020'
+	},
+	{
+		id: 2,
+		title: 'Motivation Is The First Step To Success',
+		urlToImage: './src/img/images/image2.jpg',
+		tags: ['Culture'],
+		content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+		date: '01.01.2020'
+	},
+	{
+		id: 3,
+		title: 'Success Steps For Your Personal Or Business Life',
+		urlToImage: './src/img/images/image3.jpg',
+		tags: ['Culture', 'Design', 'Art'],
+		content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+		date: '01.01.2020'
+	}
+];
 
 
 window.onload = function () {
 	console.log('ras ras');
 
+	// render articles
+	if(data) {
+		renderArticlesToDom();
+	}
+
 	// tags
 	addTagsClickHandler();
+
+	// generate base odal fro modal class
+	addToolsClickHandler();
 }
 
 const addTagsClickHandler = () => {
@@ -55,4 +90,62 @@ const filterStrategyBySelectedTag = (selectedTag) => {
 			}
 		})
 	})
+}
+
+const renderArticlesToDom = () => {
+	let strategiesWrapper = getStrategiesWrapper();
+	generateArticles(data).forEach(article => {
+		strategiesWrapper.append(article.generateArticle())
+	})
+
+	addStrategyClickHandler();
+}
+
+const getStrategiesWrapper = () => {
+	let strategiesContainer = document.querySelector('.strategy-wrapper');
+	strategiesContainer.innerHTML = '';
+	return strategiesContainer
+}
+
+const generateArticles = (data) => {
+	let articles = [];
+	data.forEach(article => {
+		articles.push(new Article(article))
+	});
+	return articles;
+}
+
+const addToolsClickHandler = () => {
+	document.querySelector('.tools__button .button').addEventListener('click', () => {
+		generateToolsModal();
+	})
+}
+
+const generateToolsModal = () => {
+	renderModalWindow('test content for tools modal');
+}
+
+const renderModalWindow = (content) => {
+	let modal = new Modal('tools-modal');
+	modal.buildModal(content)
+}
+
+const addStrategyClickHandler = () => {
+	document.querySelector('.strategy-wrapper').addEventListener('click', (e) => {
+		if (e.target.closest('.strategy')) {
+			let clickedStrategyId = e.target.closest('.strategy').getAttribute('data-id');
+			let clickedStrategyData = getClickedData(clickedStrategyId);
+
+			renderArticleModalWindow(clickedStrategyData);
+		}
+	})
+}
+
+const getClickedData = (id) => {
+	return data.find(article => article.id == id)
+}
+
+const renderArticleModalWindow = (article) => {
+	let modal = new ArticleModal('article-modal', article);
+	modal.renderModal();
 }
